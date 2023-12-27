0d70d5f84013a79f164607774c01b7cb561588f6
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2023-12-27 22:16:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # app/__init__.py
-
 from flask import Flask
-from .extensions import db, login_manager, bcrypt
+from .extensions import db, login_manager, bcrypt, cache
 from .config import Config  # Make sure 'Config' is defined in 'config.py'
 from .models import User  # This line ensures models are imported and recognized by SQLAlchemy
 from .routes import configure_routes  # This function will set up your routes
@@ -9,10 +8,14 @@ from .routes import configure_routes  # This function will set up your routes
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    app.config['CACHE_TYPE'] = 'simple'  # You can choose 'simple', 'memcached', 'redis', etc.
 
+
+    cache.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
+
 
     @login_manager.user_loader
     def load_user(user_id):
