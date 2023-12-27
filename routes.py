3ff400a95dc860624243898ae262cae4ae991cb7
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2023-12-27 22:15:01 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,9 +1,7 @@
 # routes.py
-
-from http.client import FORBIDDEN
 from flask import abort, render_template, request, url_for, flash, redirect
 from .forms import RegistrationForm, LoginForm, StorageForm
-from .models import User, Storage
+from .models import User, Storage, get_main_storage_data
 from .extensions import db, bcrypt, cache
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -69,10 +67,17 @@ def configure_routes(app):
 
     @app.route('/main_storage')
     @login_required
+    @cache.cached(timeout=300)  # Cache this view for 5 minutes
     def main_storage():
      storage_items = Storage.query.filter_by(user_id=current_user.id).all()
      return render_template('main_storage.html', storage_items=storage_items)
-
+    
+    @cache.cached(timeout=300, key_prefix='main_storage_data')
+    def get_main_storage_data_cached():
+     # This function will be cached, so it only retrieves data from the database
+     # if the cache has expired or is empty
+     return get_main_storage_data()
+    
     @app.route('/storages')
     @login_required
     def view_storages():
@@ -84,13 +89,24 @@ def configure_routes(app):
      return render_template('403.html'), 403  # Ensure you have a 403.html template
 
     @app.route('/edit_storage/<int:storage_id>', methods=['GET', 'POST'])
-    @login_required
     def edit_storage(storage_id):
      storage = Storage.query.get_or_404(storage_id)
-     if storage.user_id != current_user.id:
-        abort(403)  # HTTP Forbidden status code
-    # Present a form and handle editing the storage
-    # return render_template('edit_storage.html', storage=storage)
+     if request.method == 'POST':
+        # Assuming there's a form to process the edit
+        form = StorageForm(request.form)
+        if form.validate_on_submit():
+            # Process the validated form data and update storage
+            storage.name = form.name.data
+            # ... other fields
+            db.session.commit()
+            flash('Storage updated successfully!', 'success')
+            return redirect(url_for('view_storages', storage_id=storage.id))
+        else:
+            # Form didn't validate, fall through to the render_template call
+            flash('Error updating storage.', 'error')
+            # GET request or failed POST validation: render the edit page
+     form = StorageForm(obj=storage)
+     return render_template('edit_storage.html', form=form, storage_id=storage_id)
 
     @app.route('/delete_storage/<int:storage_id>')
     @login_required
